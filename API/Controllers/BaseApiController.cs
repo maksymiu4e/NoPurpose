b935de23ee2041b9bf59e14ae12744ba67dce404
maksymiu4e <maksymiuk.eng@gmail.com>
2024-05-29 00:40:13 +0300

    error handling added

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Application.Core;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -11,4 +12,14 @@ public class BaseApiController : ControllerBase
 
     protected IMediator Mediator => _mediator ??=
         HttpContext.RequestServices.GetService<IMediator>();
+
+    protected ActionResult HandleResult<T>(Result<T> result)
+    {
+        if (result == null) return NotFound();
+        if (result.IsSuccess && result.Value != null)
+            return Ok(result.Value);
+        if (result.IsSuccess && result.Value == null)
+            return NotFound();
+        return BadRequest(result.Error);
+    }
 }
