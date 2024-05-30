ac4e4df9d1c30d2b631354e684b79c6e01c1eaf1
maksymiu4e <maksymiuk.eng@gmail.com>
2024-05-31 00:20:18 +0300

    formik added

--- client-app/src/features/activities/details/ActivityDetailedInfo.tsx
+++ client-app/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { observer } from 'mobx-react-lite';
 import { Segment, Grid, Icon } from 'semantic-ui-react'
 import { Activity } from "../../../app/models/activity";
+import { format } from 'date-fns';
 
 interface Props {
     activity: Activity
@@ -28,7 +29,7 @@ export default observer(function ActivityDetailedInfo({ activity }: Props) {
                     </Grid.Column>
                     <Grid.Column width={15}>
                         <span>
-                            {activity.date}
+                            {format(activity.date!, 'dd MMM yyyy HH:mm')}
                         </span>
                     </Grid.Column>
                 </Grid>
