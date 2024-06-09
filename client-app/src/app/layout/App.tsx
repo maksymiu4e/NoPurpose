2f79fc176f7061ce20a468c536775ca3d58c57b6
maksymiu4e <maksymiuk.eng@gmail.com>
2024-06-09 19:14:35 +0300

    login and registration added

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
 import { Outlet, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import { ToastContainer } from 'react-toastify';
+import { useStore } from '../stores/store';
+import LoadingComponent from './LoadingComponent';
+import ModalContainer from '../common/modals/ModalContainer';
 
 function App() {
   const location = useLocation();
+  const {commonStore, userStore} = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded())
+    } else {
+      commonStore.setAppLoaded()
+    }
+  }, [commonStore, userStore])
+
+  if (!commonStore.appLoaded) return <LoadingComponent content='Loading app...' />
 
   // console error solution
   // defaultProps will be removed from function components in a future major release.
@@ -20,6 +34,7 @@ function App() {
 
   return (
     <>
+    <ModalContainer />
       <ToastContainer position='bottom-right' theme='colored' />
       {location.pathname === '/' ? <HomePage /> : (
         <>
